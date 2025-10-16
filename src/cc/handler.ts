f64a1b361f8cc4aadd7d906f3d3477da372299ce
Julien Grach <julien.grach@justice.fr>
2025-10-16 13:40:51 +0200

    with cc

---
+++ src/cc/handler.ts
@@ -0,0 +1,97 @@
+import { toUnexpectedError } from '../library/error'
+import { RawCc } from './models'
+import {
+  countFileInformations,
+  findFileInformations,
+  mapCursorSync,
+} from '../library/DbRawFile'
+import { logger } from '../library/logger'
+import { S3_BUCKET_NAME_PORTALIS } from '../library/env'
+import { updateRawFileStatus, NormalizationResult } from '../services/eventSourcing'
+import { sendToSder } from '../library/DbSder'
+
+export const rawCcToNormalize = {
+  // Ne contient pas normalized:
+  events: { $not: { $elemMatch: { type: 'normalized' } } },
+  // Les 3 derniers events ne sont pas "blocked":
+  $expr: {
+    $not: {
+      $eq: [
+        3,
+        {
+          $size: {
+            $filter: {
+              input: { $slice: ['$events', -3] },
+              as: 'e',
+              cond: { $eq: ['$$e.type', 'blocked'] }
+            }
+          }
+        }
+      ]
+    }
+  }
+}
+
+export async function normalizeCc(rawCc: RawCc): Promise<unknown> {
+  const ccDecision = rawCc.metadatas
+
+  return sendToSder(ccDecision)
+}
+
+export async function normalizeRawCcFiles(
+  defaultFilter?: Parameters<typeof findFileInformations<RawCc>>[1]
+) {
+  logger.info({
+    path: 'src/cc/handler.ts',
+    operations: ['normalization', 'normalizeRawCcFiles'],
+    message: `Starting CC normalization`
+  })
+  const _rawCcToNormalize = defaultFilter ?? rawCcToNormalize
+  const rawCcCursor = await findFileInformations<RawCc>(S3_BUCKET_NAME_PORTALIS, _rawCcToNormalize)
+  const rawCcLength = await countFileInformations<RawCc>(S3_BUCKET_NAME_PORTALIS, _rawCcToNormalize)
+  logger.info({
+    path: 'src/cc/handler.ts',
+    operations: ['normalization', 'normalizeRawCcFiles'],
+    message: `Find ${rawCcLength} raw decisions to normalize`
+  })
+
+  const results: NormalizationResult<RawCc>[] = await mapCursorSync(rawCcCursor, async (rawCc) => {
+    try {
+      logger.info({
+        path: 'src/cc/handler.ts',
+        operations: ['normalization', 'normalizeRawCcFiles'],
+        message: `normalize ${rawCc._id} - ${rawCc.path}`
+      })
+      await normalizeCc(rawCc)
+      logger.info({
+        path: 'src/cc/handler.ts',
+        operations: ['normalization', 'normalizeRawCcFiles'],
+        message: `${rawCc._id} normalized with success`
+      })
+      return { rawFile: rawCc, status: 'success' }
+    } catch (err) {
+      const error = toUnexpectedError(err)
+      logger.error({
+        path: 'src/cc/handler.ts',
+        operations: ['normalization', 'normalizeRawCcFiles'],
+        message: `${rawCc._id} failed to normalize`,
+        stack: error.stack
+      })
+      return { rawFile: rawCc, status: 'error', error }
+    }
+  })
+
+  await Promise.all(results.map(_ => updateRawFileStatus(S3_BUCKET_NAME_PORTALIS, _)))
+
+  logger.info({
+    path: 'src/cc/handler.ts',
+    operations: ['normalization', 'normalizeRawCcFiles'],
+    message: `Decisions successfully normalized: ${results.filter(({ status }) => status === 'success').length
+      }`
+  })
+  logger.info({
+    path: 'src/cc/handler.ts',
+    operations: ['normalization', 'normalizeRawCcFiles'],
+    message: `Decisions skipped: ${results.filter(({ status }) => status === 'error').length}`
+  })
+}
---
+++ src/cc/models.ts
@@ -0,0 +1,17 @@
+import { UnIdentifiedDecisionCc } from "dbsder-api-types";
+import { RawFile } from "src/services/eventSourcing";
+
+export type RawCc = RawFile<UnIdentifiedDecisionCc>
+
+export type NormalizationSucess = {
+  rawCph: RawCc
+  status: 'success'
+}
+
+export type NormalizationError = {
+  rawCph: RawCc
+  status: 'error'
+  error: Error
+}
+
+export type NormalizationResult = NormalizationError | NormalizationSucess
