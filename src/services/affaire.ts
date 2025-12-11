0a19e3b19054f8609821bce3d18c3d48dd54e219
Antoine Jeanneney <antoine.jeanneney@justice.fr>
2025-12-11 17:13:20 +0100

    documentIds is now documentAssocieIds

--- src/services/affaire.ts
+++ src/services/affaire.ts
@@ -1,22 +1,29 @@
-import { LabelStatus, parseId } from "dbsder-api-types";
-import { createAffaire, createDecision, patchDecision, UnIdentifiedDecisionSupported } from "../connectors/DbSder";
+import { LabelStatus, parseId } from 'dbsder-api-types'
+import {
+  createAffaire,
+  createDecision,
+  patchDecision,
+  UnIdentifiedDecisionSupported
+} from '../connectors/DbSder'
 
-export async function saveDecisionInAffaire(decision: UnIdentifiedDecisionSupported): Promise<unknown> {
-    const { labelStatus, ...tmpDecision } = decision
+export async function saveDecisionInAffaire(
+  decision: UnIdentifiedDecisionSupported
+): Promise<unknown> {
+  const { labelStatus, ...tmpDecision } = decision
 
-    const { _id } = await createDecision({
-        labelStatus: LabelStatus.WAITING_FOR_AFFAIRE_RESOLUTION,
-        ...tmpDecision
-    })
+  const { _id } = await createDecision({
+    labelStatus: LabelStatus.WAITING_FOR_AFFAIRE_RESOLUTION,
+    ...tmpDecision
+  })
 
-    const decisionId = parseId(_id)
-    
-    await createAffaire({
-        decisionIds: [decisionId],
-        documentIds: [],
-        replacementTerms: []
-    })
-    return patchDecision(decisionId, {
-        labelStatus,
-    })
+  const decisionId = parseId(_id)
+
+  await createAffaire({
+    decisionIds: [decisionId],
+    documentAssocieIds: [],
+    replacementTerms: []
+  })
+  return patchDecision(decisionId, {
+    labelStatus
+  })
 }
