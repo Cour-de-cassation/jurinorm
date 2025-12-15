69dc68acba5fa77dcf4ef0f3729994506d13a005
Antoine Jeanneney <antoine.jeanneney@justice.fr>
2025-12-15 17:30:32 +0100

    fix ca/cc reprocess scripts

--- src/ca/scripts/startNormalizationOnAllStock.ts
+++ src/ca/scripts/startNormalizationOnAllStock.ts
@@ -1,6 +1,8 @@
 import { disconnect } from '../../library/DbRawFile'
 import { normalizeRawCaFiles } from '../handler'
 
-normalizeRawCaFiles({ events: { $not: { $elemMatch: { type: 'normalized' } } } }).finally(() => {
+normalizeRawCaFiles({
+  events: { $not: { $elemMatch: { type: { $in: ['normalized', 'deleted'] } } } }
+}).finally(() => {
   setTimeout(disconnect, 3000)
 }) // probably useless to wait - just in case
--- src/cc/scripts/startNormalizationOnYesterday.ts
+++ src/cc/scripts/startNormalizationOnYesterday.ts
@@ -3,7 +3,7 @@ import { normalizeRawCcFiles } from '../handler'
 
 normalizeRawCcFiles({
   'events.0.date': { $gte: new Date(Date.now() - 24 * 60 * 60 * 1000) },
-  events: { $not: { $elemMatch: { type: 'normalized' } } }
+  events: { $not: { $elemMatch: { type: { $in: ['normalized', 'deleted'] } } } }
 }).finally(() => {
   setTimeout(disconnect, 3000)
 }) // probably useless to wait - just in case
