86199360f303297d12b3107f88f3374c551b7c88
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2026-01-19 13:48:06 +0100

    Fix guillemets before removing consecutive spaces

--- src/tj/batch/normalization/services/removeOrReplaceUnnecessaryCharacters.ts
+++ src/tj/batch/normalization/services/removeOrReplaceUnnecessaryCharacters.ts
@@ -13,13 +13,16 @@ export const replaceUnknownCharacters = (text: string) => {
 }
 
 export const removeOrReplaceUnnecessaryCharacters = (rawString: string): string => {
+  // Fix guillemets:
+  const stringWithFixedGuillemets = fixGuillemets(rawString)
+
   // Regular expressions to remove specific characters
   const tabOrPageBreakRegex = /\t|\f/gi
   const carriageReturnRegex = /\r\n|\r/gi
   const multipleSpaceRegex = /[ ]{2,}/gi
 
   // Replace tab or pageBreak characters with an empty string
-  const stringWithoutTabOrPageBreak = rawString.replace(tabOrPageBreakRegex, ' ')
+  const stringWithoutTabOrPageBreak = stringWithFixedGuillemets.replace(tabOrPageBreakRegex, ' ')
 
   // Replace carriageReturn characters with a newline character
   const stringWithoutCarriageReturn = stringWithoutTabOrPageBreak.replace(carriageReturnRegex, '\n')
@@ -30,7 +33,7 @@ export const removeOrReplaceUnnecessaryCharacters = (rawString: string): string 
     ' '
   )
 
-  //replace tibetain characters
+  // Replace tibetain characters
   const normalizedText = replaceUnknownCharacters(stringWithoutConsecutiveSpaces)
 
   return normalizedText.trim()
@@ -44,4 +47,57 @@ export function isEmptyText(text: string): boolean {
 export function hasNoBreak(text: string): boolean {
   const hasBreak = `${text}`.includes('\n')
   return hasBreak === false
+}
+
+function fixGuillemets(originalText: string): string {
+  // Guillemets tend to be corrupted in the received WordPerfect file
+  // (this issue affects more than half of the decisions):
+  // - the opening '«' becomes a tibetan character 'ཋ' (code 3915)
+  // - the closing '»' becomes a regular space character (code 32)
+  //
+  // Once a 'ཋ' character is found, we look for the next occurrence of a
+  // double space string (32+32) in order to reconstruct the initial quote.
+  //
+  // If the current quote cannot be closed, its opening 'ཋ' characters is
+  // replaced with a space character.
+  //
+  // We have to parse the whole text *before* the removing of all consecutive
+  // spaces (stringWithoutConsecutiveSpaces), otherwise we lose the
+  // end-of-quote condition.
+  //
+  // The nature of the quoted text makes the use of regexps quite uncertain.
+
+  let text = originalText
+  let beginQuoteIndex = -1
+
+  for (let i = 0; i < text.length; i++) {
+    const charCode = text.charCodeAt(i)
+    if (charCode === 3915) {
+      // Found a 'ཋ'
+      if (beginQuoteIndex !== -1) {
+        // Unexpected 'ཋ': remove the previous "unclosed" 'ཋ' and continue with the new one
+        text = text.substring(0, beginQuoteIndex) + ' ' + text.substring(beginQuoteIndex + 1)
+      }
+      beginQuoteIndex = i
+    } else if (
+      charCode === 32 &&
+      i < text.length - 1 &&
+      beginQuoteIndex !== -1 &&
+      text.charCodeAt(i + 1) === 32
+    ) {
+      // Found a double space after a 'ཋ':
+      // - replace the 'ཋ' with a '«'
+      // - replace the second space with a '»'
+      text = text.substring(0, beginQuoteIndex) + '«' + text.substring(beginQuoteIndex + 1)
+      text = text.substring(0, i + 1) + '»' + text.substring(i + 2)
+      beginQuoteIndex = -1
+    }
+  }
+
+  if (beginQuoteIndex !== -1) {
+    // End of text: remove the remaining "unclosed" 'ཋ'
+    text = text.substring(0, beginQuoteIndex) + ' ' + text.substring(beginQuoteIndex + 1)
+  }
+
+  return text
 }
