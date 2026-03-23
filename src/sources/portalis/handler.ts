b81b4736b4f5fe48b92316b59104c91c25a04f48
Julien Grach <julien.grach@justice.fr>
2026-03-23 16:14:13 +0100

    add filter CPH to portalis & rename cph to portalis

--- src/sources/portalis/handler.ts
+++ src/sources/portalis/handler.ts
@@ -1,69 +1,69 @@
 import { toUnexpectedError } from '../../services/error'
-import { RawCph } from './models'
+import { RawPortalis } from './models'
 import {
   countFileInformations,
   findFileInformations,
   mapCursorSync
 } from '../../connectors/dbRawFile'
-import { normalizeCph, rawCphToNormalize } from './normalization'
+import { normalizePortalis, rawPortalisToNormalize } from './normalization'
 import { logger } from '../../config/logger'
 import { S3_BUCKET_NAME_PORTALIS } from '../../config/env'
 import { updateRawFileStatus, NormalizationResult } from '../../services/eventSourcing'
 
-export async function normalizeRawCphFiles(
-  defaultFilter?: Parameters<typeof findFileInformations<RawCph>>[1],
+export async function normalizeRawPortalisFiles(
+  defaultFilter?: Parameters<typeof findFileInformations<RawPortalis>>[1],
   limit?: number
 ) {
   logger.info({
-    path: 'src/service/cph/handler.ts',
-    operations: ['normalization', 'normalizeRawCphFiles'],
-    message: `Starting CPH normalization`
+    path: 'src/service/sources/portalis/handler.ts',
+    operations: ['normalization', 'normalizeRawPortalisFiles'],
+    message: `Starting Portalis normalization`
   })
-  const _rawCphToNormalize = defaultFilter ?? rawCphToNormalize
-  const rawCphCursor = await findFileInformations<RawCph>(
+  const rawPortalisFilter = defaultFilter ?? rawPortalisToNormalize
+  const rawPortalisCursor = await findFileInformations<RawPortalis>(
     S3_BUCKET_NAME_PORTALIS,
-    _rawCphToNormalize,
+    rawPortalisFilter,
     limit
   )
-  const rawCphLength = await countFileInformations<RawCph>(
+  const rawPortalisLength = await countFileInformations<RawPortalis>(
     S3_BUCKET_NAME_PORTALIS,
-    _rawCphToNormalize
+    rawPortalisFilter
   )
   logger.info({
-    path: 'src/service/cph/handler.ts',
-    operations: ['normalization', 'normalizeRawCphFiles'],
-    message: `Find ${rawCphLength} raw decisions to normalize batch. Limit is set to ${limit}`
+    path: 'src/service/sources/portalis/handler.ts',
+    operations: ['normalization', 'normalizeRawPortalisFiles'],
+    message: `Find ${rawPortalisLength} raw decisions to normalize batch. Limit is set to ${limit}`
   })
 
-  const results: NormalizationResult<RawCph>[] = await mapCursorSync(
-    rawCphCursor,
-    async (rawCph) => {
+  const results: NormalizationResult<RawPortalis>[] = await mapCursorSync(
+    rawPortalisCursor,
+    async (rawPortalis) => {
       try {
         logger.info({
-          path: 'src/service/cph/handler.ts',
-          operations: ['normalization', 'normalizeRawCphFiles'],
-          message: `normalize ${rawCph._id} - ${rawCph.path}`
+          path: 'src/service/sources/portalis/handler.ts',
+          operations: ['normalization', 'normalizeRawPortalisFiles'],
+          message: `normalize ${rawPortalis._id} - ${rawPortalis.path}`
         })
-        await normalizeCph(rawCph)
+        await normalizePortalis(rawPortalis)
         logger.info({
-          path: 'src/service/cph/handler.ts',
-          operations: ['normalization', 'normalizeRawCphFiles'],
-          message: `${rawCph._id} normalized with success`
+          path: 'src/service/sources/portalis/handler.ts',
+          operations: ['normalization', 'normalizeRawPortalisFiles'],
+          message: `${rawPortalis._id} normalized with success`
         })
 
-        const result = { rawFile: rawCph, status: 'success' } as const
+        const result = { rawFile: rawPortalis, status: 'success' } as const
         await updateRawFileStatus(S3_BUCKET_NAME_PORTALIS, result)
         return result
       } catch (err) {
         const error = toUnexpectedError(err)
         logger.error({
-          path: 'src/service/cph/handler.ts',
-          operations: ['normalization', 'normalizeRawCphFiles'],
-          message: `${rawCph._id} failed to normalize`,
+          path: 'src/service/sources/portalis/handler.ts',
+          operations: ['normalization', 'normalizeRawPortalisFiles'],
+          message: `${rawPortalis._id} failed to normalize`,
           stack: error.stack
         })
 
-        const result = { rawFile: rawCph, status: 'error', error } as const
+        const result = { rawFile: rawPortalis, status: 'error', error } as const
         await updateRawFileStatus(S3_BUCKET_NAME_PORTALIS, result)
         return result
       }
@@ -73,15 +73,15 @@ export async function normalizeRawCphFiles(
   await Promise.all(results)
 
   logger.info({
-    path: 'src/service/cph/handler.ts',
-    operations: ['normalization', 'normalizeRawCphFiles'],
+    path: 'src/service/sources/portalis/handler.ts',
+    operations: ['normalization', 'normalizeRawPortalisFiles'],
     message: `Decisions successfully normalized: ${
       results.filter(({ status }) => status === 'success').length
     }`
   })
   logger.info({
-    path: 'src/service/cph/handler.ts',
-    operations: ['normalization', 'normalizeRawCphFiles'],
+    path: 'src/service/sources/portalis/handler.ts',
+    operations: ['normalization', 'normalizeRawPortalisFiles'],
     message: `Decisions skipped: ${results.filter(({ status }) => status === 'error').length}`
   })
 }
