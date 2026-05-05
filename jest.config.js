f4ca53bfe24eabca797c150b1794a22449167b51
fred <frederic.thomas-hubert@justice.fr>
2026-05-05 11:50:36 +0200

    Ajout tests sur décisions TJ + path aliases

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,14 @@ module.exports = {
   testMatch: [
     "<rootDir>/src/**/*.spec.ts",
   ],
+  moduleNameMapper: {
+    "^@ca/(.*)$": "<rootDir>/src/sources/jurica/$1",
+    "^@cc/(.*)$": "<rootDir>/src/sources/jurinet/$1",
+    "^@connectors/(.*)$": "<rootDir>/src/connectors/$1",
+    "^@cph/(.*)$": "<rootDir>/src//sources/portalis/$1",
+    "^@services/(.*)$": "<rootDir>/src/services/$1",
+    "^@tcom/(.*)$": "<rootDir>/src/sources/juritcom/$1",
+    "^@tj/(.*)$": "<rootDir>/src/sources/juritj/$1",
+    "^@config/(.*)$": "<rootDir>/src/config/$1"
+  }
 };
