f7ba0b20c8c90c990ce3702fae824adb3c0c9584
Julien Grach <julien.grach@justice.fr>
2026-05-20 16:55:43 +0200

    remove swagger

--- src/sources/juritcom/shared/infrastructure/dto/receive.dto.ts
+++ src/sources/juritcom/shared/infrastructure/dto/receive.dto.ts
@@ -1,26 +1,12 @@
-import { ApiProperty } from '@nestjs/swagger'
 import { MetadonneeDto } from './metadonnee.dto'
 import { IsString } from 'class-validator'
 
 export class ReceiveDto {
-  @ApiProperty({
-    type: 'string',
-    format: 'binary',
-    description: 'Décision intègre au format PDF.'
-  })
   fichierDecisionIntegre: Express.Multer.File
 
-  @ApiProperty({
-    description: 'Texte de la décision intègre',
-    type: String,
-    example: 'Texte de la décision'
-  })
   @IsString()
   texteDecisionIntegre: string
 
-  @ApiProperty({
-    description: 'Metadonnées associées à la décision intègre.'
-  })
   metadonnees: MetadonneeDto
 }
 
