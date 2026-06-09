ec11a56b02d07650dacc9bb4b71c35cc1f03ee0a
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2026-06-09 17:04:59 +0200

    testing test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  setupFiles: ["<rootDir>/src/.jest/setupEnvVars.ts"],
+  setupFiles: ["./src/.jest/setupEnvVars.ts"],
   testEnvironment: "node",
   transform: {
     "^.+\.tsx?$": ["ts-jest",{}],
